8c99ea37a2014fdb41cde0b752015289b579caa6
VaibhavDhamanage <vaibhavdhamanage>
2024-03-09 10:32:34 +0530

    add varcel dependence

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import NavBar from "@/component/Navbar/NavBar";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }) {
   return (
