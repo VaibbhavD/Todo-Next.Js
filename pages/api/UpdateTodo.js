a562c53b4d0d4f3352d9dde9676414ec1998b4cc
VaibhavDhamanage <vaibhavdhamanage>
2024-03-09 10:13:14 +0530

    add updattodo component

---
+++ pages/api/UpdateTodo.js
@@ -0,0 +1,18 @@
+import { MongoClient, ObjectId } from "mongodb";
+export default async function Post(req, res) {
+  if (req.method === "PUT") {
+    const data = req.body;
+    const Client = await MongoClient.connect(
+      "mongodb+srv://vaibhavdhamanage12:FnNmAenYGVoZKLCK@cluster0.t5bx9cq.mongodb.net/Todo?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    const db = Client.db();
+    const Todocollection = db.collection("Todo");
+    await Todocollection.updateOne(
+      { _id: new ObjectId(data.id) },
+      { $set: { todo: data.todo } }
+    );
+    Client.close();
+
+    res.status(201).json({ message: "Update Successfull !" });
+  }
+}
